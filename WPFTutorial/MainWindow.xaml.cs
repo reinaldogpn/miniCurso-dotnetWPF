dcaf20650ab4d5205a18e2a9dde57d18daa62f9f
Reinaldo Neto <reinaldogpn@outlook.com>
2024-07-01 21:23:39 -0300

    Code behind

--- WPFTutorial/MainWindow.xaml.cs
+++ WPFTutorial/MainWindow.xaml.cs
@@ -4,9 +4,27 @@ namespace WPFTutorial
 {
     public partial class MainWindow : Window
     {
+        public bool IsRunning { get; set; } = true;
         public MainWindow()
         {
             InitializeComponent();
+
+            tbHello.Text = "Hello World 2!";
+            btnToggleRun.Content = "Run";
+        }
+
+        private void btnToggleRun_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsRunning)
+            {
+                btnToggleRun.Content = "Running";
+            }
+            else
+            {
+                btnToggleRun.Content = "Stopped";
+            }
+
+            IsRunning = !IsRunning;
         }
     }
 }
