5c47d2630d0a7482fbecabaa2cc4d29194da44cd
Reinaldo Neto <reinaldogpn@outlook.com>
2024-07-02 01:17:10 -0300

    Grid control for basic responsive layouts

--- WPFTutorial/MainWindow.xaml.cs
+++ WPFTutorial/MainWindow.xaml.cs
@@ -8,23 +8,6 @@ namespace WPFTutorial
         public MainWindow()
         {
             InitializeComponent();
-
-            tbHello.Text = "Hello World 2!";
-            btnToggleRun.Content = "Run";
-        }
-
-        private void btnToggleRun_Click(object sender, RoutedEventArgs e)
-        {
-            if (IsRunning)
-            {
-                btnToggleRun.Content = "Running";
-            }
-            else
-            {
-                btnToggleRun.Content = "Stopped";
-            }
-
-            IsRunning = !IsRunning;
         }
     }
 }
