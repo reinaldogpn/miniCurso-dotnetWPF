0bedbf83c54dfa80a42d87b7afdddafda7d28bfc
Reinaldo Neto <reinaldogpn@outlook.com>
2024-07-02 02:25:01 -0300

    Data bindings using INotifyPropertyChanged

--- WPFTutorial/MainWindow.xaml.cs
+++ WPFTutorial/MainWindow.xaml.cs
@@ -1,13 +1,40 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
 
 namespace WPFTutorial
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public bool IsRunning { get; set; } = true;
         public MainWindow()
         {
+            DataContext = this;
             InitializeComponent();
+        }
+
+        private string boundText;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string BoundText
+        {
+            get { return boundText; }
+            set
+            {
+                boundText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void btnSet_Click(object sender, RoutedEventArgs e)
+        {
+            BoundText = "set from code";
+        }
+
+        private void OnPropertyChanged( [CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
